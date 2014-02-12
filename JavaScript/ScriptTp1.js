433de8eff602e97aed0306371ff1ee96e485cde7
Daren-Ken St-laurent <daren511@hotmail.com>
2014-02-12 09:17:12 -0500

    bs

--- JavaScript/ScriptTp1.js
+++ JavaScript/ScriptTp1.js
@@ -5,10 +5,19 @@ function lireUneTouche(event) {
 }
 document.addEventListener('keypress', lireUneTouche);
 
-function lireUnbackspace(event) {
+function lireSpecial(event) {
+	//BS
 	var dir = document.getElementById("Editeur");
 	if(event.keyCode == '8' && dir.innerHTML.length >= 1){
-		dir.innerHTML.substring(0,dir.innerHTML.length-1);
+		dir.innerHTML = dir.innerHTML.substring(0,dir.innerHTML.length-1);
 	}
 }
-document.addEventListener('keydown',lireUnbackspace);
+document.addEventListener('keydown',lireSpecial);
+
+function Keywords(){
+	return ['break','case','catch','continue','debugger','default','delete','do','else','finally','for','function','if','in','instanceof','new','return','switch','this','throw','try','typeof','var','void','while','with']
+}
+function isSpace(c) {
+   return c == ' '  || c == '\n' || c == '\r' ||
+          c == '\f' || c == '\t' || c == '\v';
+}
