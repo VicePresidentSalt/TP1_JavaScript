890240914ac5b02ec49e10e25efe2cb86ab74f63
Daren-Ken St-laurent <daren511@hotmail.com>
2014-02-12 11:38:43 -0500

    css+js

--- JavaScript/ScriptTp1.js
+++ JavaScript/ScriptTp1.js
@@ -62,25 +62,13 @@ function lireSpecial(event) {
 	//BS
 	var dir = document.getElementById("Editeur");
 
-	if(event.keyCode == '8' && dir.innerHTML.length >= 1){
-		dir.innerHTML = dir.innerHTML.substring(0,dir.innerHTML.length-1);
+if(event.keyCode == 8 && dir.textContent.length >= 1){
+		dir.innerHTML=formater(dir.textContent.substring(0,dir.textContent.length-1));
 	}
 }
 document.addEventListener('keydown',lireSpecial);
 
-function Keywords(){
-	return ['break','case','catch','continue','debugger','default','delete','do','else','finally','for','function','if','in','instanceof','new','return','switch','this','throw','try','typeof','var','void','while','with']
-}
-function isSpace(c) {
-   return c == ' '  || c == '\n' || c == '\r' ||
-          c == '\f' || c == '\t' || c == '\v';
-}
-	if(event.keyCode == 8 && dir.textContent.length >= 1){
-		dir.innerHTML=formater(dir.textContent.substring(0,dir.textContent.length-1));
-	}
-}
 document.addEventListener('keydown',lireUnbackspace);
-
 
 function peinturerTexte() {
    var zone = document.getElementById("zoneCode");
