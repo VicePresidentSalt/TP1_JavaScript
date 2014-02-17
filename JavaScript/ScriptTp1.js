05d8d1a99abef80509bff5017425845f839d8373
hcfranck <hc_throwdown@hotmail.com>
2014-02-17 17:32:43 -0500

    brup brup

--- JavaScript/ScriptTp1.js
+++ JavaScript/ScriptTp1.js
@@ -1,19 +1,14 @@
 // JavaScript source code
-function isSpace(c) {
-   return c == ' '  || c == '\n' || c == '\r' ||
-          c == '\f' || c == '\t' || c == '\v';
+function keyWord() {
+   return [ "break", "case", "catch", "continue", "debugger", "default", "delete", "do", 
+   "else", "finally", "for", "function", "if", "in", "instanceof", "new", "return", "switch", 
+   "this", "throw", "try", "typeof", "var", "void", "while", "with" ];
 }
-function négation(f) {
-   return function(arg) {
-      return !f(arg);
-   };
+function formaterTexte() {
+   var zone = document.getElementById("zoneCode");
+   zone.innerHTML = formater(zone.innerText);
 }
-function trouverSi(s,pos,f) {
-   while(pos != s.length && !f(s[pos])) {
-      ++pos;
-   }
-   return pos;
-}
+
 function subStr(s,début,fin) { // suppose début et fin Ok
    var résultat = "";
    for(; début != fin; ++début) {
@@ -21,13 +16,18 @@ function subStr(s,début,fin) { // suppose début et fin Ok
    }
    return résultat;
 }
-function keyWord() {
-   return [ "break", "case", "catch", "continue", "debugger", "default", "delete", "do", 
-   "else", "finally", "for", "function", "if", "in", "instanceof", "new", "return", "switch", 
-   "this", "throw", "try", "typeof", "var", "void", "while", "with" ];
-}
+
+function trouverDans(val,tab) {
+   for(var i = 0; i != tab.length; ++i) {
+      if (val == tab[i]) {
+         return i;
+      }
+   }
+return tab.length;
+
+
 function formater(s) {
-   var temp = "";
+   var mots = keyWord();
    var résultat = "";
    var pos = 0;
    while (pos != s.length) {
@@ -38,23 +38,46 @@ function formater(s) {
       // consommer mot
       if (pos != s.length) {
          prochain = trouverSi(s,pos,isSpace);
-         temp = subStr(s,pos,prochain);
-         if(temp == keyWord()){
-
+         //résultat += "<u>" + s[pos] + "</u>";
+         //résultat += subStr(s,pos+1,prochain);
+         var mot = subStr(s,pos,prochain);
+         if (trouverDans(mot,mots) != mots.length) {
+            résultat += "<strong>" + mot + "</strong>";
+         } else {
+            résultat += mot;
          }
          pos = prochain;
-         résultat += temp;
+      }
       }
    }
    return résultat;
 }
 
+function isSpace(c) {
+   return c == ' '  || c == '\n' || c == '\r' ||
+          c == '\f' || c == '\t' || c == '\v';
+}
+
+function négation(f) {
+   return function(arg) {
+      return !f(arg);
+   };
+}
+
+function trouverSi(s,pos,f) {
+   while(pos != s.length && !f(s[pos])) {
+      ++pos;
+   }
+   return pos;
+}
+
+
+
 function lireUneTouche(event) {
   
   var dir = document.getElementById("Editeur");	
   
-  dir.innerHTML = formater(dir.textContent + String.fromCharCode(event.which));
-  
+  dir.innerHTML = formater(dir.textContent + String.fromCharCode(event.which)); 
 }
 document.addEventListener('keypress', lireUneTouche);
 
@@ -70,8 +93,11 @@ document.addEventListener('keydown',lireSpecial);
 
 document.addEventListener('keydown',lireUnbackspace);
 
+<<<<<<< HEAD
 function peinturerTexte() {
    var zone = document.getElementById("zoneCode");
    zone.innerHTML = peinturer(zone.innerText);
 }
 
+=======
+>>>>>>> brup brup
